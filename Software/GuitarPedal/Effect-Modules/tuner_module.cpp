cdc70764adbbf7417d26f0cfc3caccb90a354770
Sean Kelly <sevendaystillsean@gmail.com>
2024-10-10 18:26:14 -0700

    buffer doesnt fit in memory?

--- Software/GuitarPedal/Effect-Modules/tuner_module.cpp
+++ Software/GuitarPedal/Effect-Modules/tuner_module.cpp
@@ -8,6 +8,69 @@
 using namespace soundmath;
 using namespace bkshepherd;
 
+////
+#define PI 3.1415926535897932384626433832795
+#define SR 48000
+typedef float S;  // sample type
+
+using namespace daisy;
+using namespace daisysp;
+using namespace soundmath;
+
+// convenient lookup tables
+Wave<S> hann([](S phase) -> S { return 0.5 * (1 - cos(2 * PI * phase)); });
+Wave<S> halfhann([](S phase) -> S { return sin(PI * phase); });
+
+// 4 overlapping windows of size 2^12 = 4096
+const size_t order = 12;
+const size_t N = (1 << order);
+const S sqrtN = sqrt(N);
+const size_t laps = 4;
+const size_t buffsize = 2 * laps * N;
+
+// buffers for STFT processing
+// audio --> in --(fft)--> middle --(process)--> out --(ifft)--> in -->
+// each of these is a few circular buffers stacked end-to-end.
+S in[buffsize];  // buffers for input and output (from / to user audio callback)
+S middle[buffsize];  // buffers for unprocessed frequency domain data
+S out[buffsize];     // buffers for processed frequency domain data
+
+ShyFFT<S, N, RotationPhasor>* fft;  // fft object
+Fourier<S, N>* stft;                // stft object
+
+// initial parameters for denoise process
+// if testing without hardware control, try changing these values
+// beta = mix between high- and low-energy frequency bands
+// thresh = cutoff for designation of a bin as high- or low-energy
+S beta = 1, thresh = 15;
+// shy_fft packs arrays as [real, real, real, ..., imag, imag, imag, ...]
+inline void denoise(const S* in, S* out) {
+  // convenient constant for grabbing imaginary parts
+  static const size_t offset = N / 2;
+
+  S average = 0;
+  for (size_t i = 0; i < N; i++) {
+    out[i] = 0;
+    average += in[i] * in[i];
+  }
+
+  average /= N;
+
+  for (size_t i = 0; i < N / 2; i++) {
+    if ((in[i] * in[i] + in[i + offset] * in[i + offset]) <
+        thresh * thresh * average) {
+      // rescale the low-amplitude frequency bins by (1 - beta) ...
+      out[i] = (1 - beta) * in[i];
+      out[i + offset] = (1 - beta) * in[i + offset];
+    } else {
+      // ... and the high-amplitude ones by beta
+      out[i] = beta * in[i];
+      out[i + offset] = beta * in[i + offset];
+    }
+  }
+}
+////
+
 const char k_notes[12][3] = {"C",  "C#", "D",  "D#", "E",  "F",
                              "F#", "G",  "G#", "A",  "A#", "B"};
 
@@ -26,11 +89,17 @@ TunerModule::TunerModule() : BaseEffectModule() {
 
   // Initialize Parameters for this Effect
   this->InitParams(0);
+
+  // initialize FFT and STFT objects
+  fft = new ShyFFT<S, N, RotationPhasor>();
+  fft->Init();
+  stft = new Fourier<S, N>(denoise, fft, &hann, laps, in, middle, out);
 }
 
 // Destructor
 TunerModule::~TunerModule() {
-  // No Code Needed
+  delete fft;
+  delete stft;
 }
 
 void TunerModule::Init(float sample_rate) {
@@ -65,7 +134,7 @@ void TunerModule::ProcessMono(float in) {
 
 void TunerModule::ProcessStereo(float inL, float inR) { ProcessMono(inL); }
 
-void TunerModule::DrawUI(OneBitGraphicsDisplay &display, int currentIndex,
+void TunerModule::DrawUI(OneBitGraphicsDisplay& display, int currentIndex,
                          int numItemsTotal, Rectangle boundsToDrawIn,
                          bool isEditing) {
   BaseEffectModule::DrawUI(display, currentIndex, numItemsTotal, boundsToDrawIn,
