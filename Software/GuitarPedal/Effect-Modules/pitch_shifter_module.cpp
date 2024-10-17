6051cd30782e6dbdbbf5a7983ddcbbf8124a4a08
Sean Kelly <sevendaystillsean@gmail.com>
2024-10-16 21:04:03 -0700

    leave crossfade

--- Software/GuitarPedal/Effect-Modules/pitch_shifter_module.cpp
+++ Software/GuitarPedal/Effect-Modules/pitch_shifter_module.cpp
@@ -22,6 +22,7 @@ static const ParameterMetaData s_metaData[s_paramCount] = {
 };
 
 static daisysp_modified::PitchShifter DSY_SDRAM_BSS pitchShifter;
+static CrossFade pitchCrossfade;
 
 // Default Constructor
 PitchShifterModule::PitchShifterModule() : BaseEffectModule() {
@@ -44,6 +45,10 @@ void PitchShifterModule::Init(float sample_rate) {
 
   const int semitone = GetParameterAsBinnedValue(0);
   pitchShifter.SetTransposition((float)semitone);
+
+  pitchCrossfade.Init(CROSSFADE_CPOW);
+
+  // TODO SK: Fix loud noise at startup
 }
 
 void PitchShifterModule::ParameterChanged(int parameter_id) {
@@ -58,7 +63,8 @@ void PitchShifterModule::ProcessMono(float in) {
   BaseEffectModule::ProcessMono(in);
 
   float shifted = pitchShifter.Process(in);
-  m_audioRight = m_audioLeft = shifted;
+  const float out = pitchCrossfade.Process(in, shifted);
+  m_audioRight = m_audioLeft = out;
 }
 
 void PitchShifterModule::ProcessStereo(float inL, float inR) {
