8f241726978fb10e1fb52cb7e8ea8af9b26dda49
Sean Kelly <sevendaystillsean@gmail.com>
2024-10-26 14:37:23 -0700

    More UI work on the tuner

--- Software/GuitarPedal/Effect-Modules/tuner_module.cpp
+++ Software/GuitarPedal/Effect-Modules/tuner_module.cpp
@@ -1,10 +1,9 @@
 #include "tuner_module.h"
 
+#include <q/fx/lowpass.hpp>
 #include <q/fx/signal_conditioner.hpp>
 #include <q/pitch/pitch_detector.hpp>
 #include <q/support/pitch_names.hpp>
-
-#include "q/fx/lowpass.hpp"
 
 using namespace bkshepherd;
 
@@ -18,8 +17,6 @@ const char k_notes[12][3] = {"C",  "C#", "D",  "D#", "E",  "F",
 const uint16_t k_screenWidth = 128;
 
 static const ParameterMetaData s_metaData[0] = {};
-
-dynamic_smoother smoothingFilter{0.3, 48000};
 
 // Default Constructor
 TunerModule::TunerModule() : BaseEffectModule() {
@@ -39,6 +36,9 @@ TunerModule::~TunerModule() {
 
   delete m_preProcessor;
   m_preProcessor = nullptr;
+
+  delete m_smoothingFilter;
+  m_smoothingFilter = nullptr;
 }
 
 void TunerModule::Init(float sample_rate) {
@@ -54,6 +54,8 @@ void TunerModule::Init(float sample_rate) {
   signal_conditioner::config preprocessor_config;
   m_preProcessor = new signal_conditioner{preprocessor_config, lowest_frequency,
                                           highest_frequency, sample_rate};
+
+  m_smoothingFilter = new dynamic_smoother{20, sample_rate};
 }
 
 float Pitch(uint8_t note) { return 440.0f * pow(2.0f, (note - 'E') / 12.0f); }
@@ -69,16 +71,19 @@ uint8_t Note(float frequency) {
 uint8_t Octave(float frequency) { return Note(frequency) / 12.0f - 1.0f; }
 
 void TunerModule::ProcessMono(float in) {
-  // Run the detector
   // Pre-process the signal for pitch detection
-  float pd_sig = m_preProcessor->operator()(in);
+  float preProcessedSignal = m_preProcessor->operator()(in);
 
-  // send the processed sample through the pitch detector
-  m_pitchDetector->operator()(pd_sig);
-  m_currentFrequency = as_float(frequency{m_pitchDetector->get_frequency()});
+  // Send the processed sample through the pitch detector
+  const bool ready = m_pitchDetector->operator()(preProcessedSignal);
 
-  // Try to get the latest frequency from the detector
-  // m_currentFrequency = smoothingFilter(m_currentFrequency);
+  // If result is ready, get the detected frequency
+  if (ready) {
+    m_currentFrequency = m_pitchDetector->get_frequency();
+
+    // Run a smoothing filter on the detected frequency
+    m_currentFrequency = m_smoothingFilter->operator()(m_currentFrequency);
+  }
 
   m_note = Note(m_currentFrequency);
   m_octave = Octave(m_currentFrequency);
@@ -98,42 +103,67 @@ void TunerModule::DrawUI(OneBitGraphicsDisplay& display, int currentIndex,
   display.WriteStringAligned(strbuffNote, Font_16x26, boundsToDrawIn,
                              Alignment::topCentered, true);
 
+  // This has to be an odd number so the middle block is "in tune"
+  const uint8_t blockCount = 11;
+  const uint8_t inTuneBlockIndex = (blockCount - 1) / 2;
+  const uint8_t numBlocksOutOfTune = (blockCount - 1) / 2;
+
   // Thresholds for tuning accuracy mapping to blocks
-  const float close = 1.0f;
-  const float medium = 3.0f;
-  const float far = 10.0f;
+  const float closeThreshold = 1.0f;
+  const float farLimit = 10.0f;
 
-  const int blockCount = 7;
-  bool blockActive[blockCount] = {false};
+  // 0 is in tune, 1.0f is max out of tune we display, cents sign is used to
+  // determine sharp or flat
+  float percentage = m_cents / farLimit;
+  percentage = std::clamp(percentage, 0.0f, 1.0f);
 
-  blockActive[3] = true;
-
-  if (m_cents < -far) {
-    blockActive[0] = true;
-    blockActive[1] = true;
-    blockActive[2] = true;
-  } else if (m_cents < -medium) {
-    blockActive[1] = true;
-    blockActive[2] = true;
-  } else if (m_cents < -close) {
-    blockActive[2] = true;
-  } else if (m_cents > far) {
-    blockActive[4] = true;
-    blockActive[5] = true;
-    blockActive[6] = true;
-  } else if (m_cents > medium) {
-    blockActive[4] = true;
-    blockActive[5] = true;
-  } else if (m_cents > close) {
-    blockActive[4] = true;
-  }
+  const uint8_t numBlocksOutOfTuneToDisplay =
+      static_cast<float>(numBlocksOutOfTune) * std::abs(percentage);
 
   int width = boundsToDrawIn.GetWidth();
   int blockWidth = (width / blockCount);
   int top = 30;
 
+  bool blockActive[blockCount] = {false};
+
+  // The center block is always active
+  blockActive[inTuneBlockIndex] = true;
+
+  // Counter used to set the blocks (gets decremented as we set them active)
+  // We cap this to always show at least 1, and we will remove that 1 in the
+  // next step if our tuning threshold is met
+  uint8_t blockChangeCount =
+      std::max(numBlocksOutOfTuneToDisplay, static_cast<uint8_t>(1));
+
+  // Only show no blocks out of tune if we are within the close threshold
+  if (std::abs(m_cents) < closeThreshold) {
+    blockChangeCount = 0;
+  }
+
+  if (m_cents < 0) {
+    // Set the flat blocks state
+    for (int i = inTuneBlockIndex - 1; i >= 0; i--) {
+      if (blockChangeCount > 0) {
+        blockActive[i] = true;
+        blockChangeCount--;
+      } else {
+        break;
+      }
+    }
+  } else {
+    // Set the sharp blocks state
+    for (int i = inTuneBlockIndex + 1; i < blockCount; i++) {
+      if (blockChangeCount > 0) {
+        blockActive[i] = true;
+        blockChangeCount--;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // Display all of the blocks
   int x = 0;
-  // Draw all of the active blocks
   for (int block = 0; block < blockCount; block++) {
     Rectangle r(x, top, blockWidth, blockWidth);
     display.DrawRect(r, true, blockActive[block]);
